4a04203644ab48a7eca7fbf99f3a14d7d9eefa74
Abiyyu <abiyyu18@gmail.com>
2026-06-09 13:55:43 +0800

    feat: menambahkan view fakultas

---
+++ application/views/fakultas/index.php
@@ -0,0 +1,26 @@
+<div class="card shadow border-0 mb-4">
+	<div class="card-header bg-secondary text-white d-flex flex-column flex-md-row justify-content-between align-items-md-center gap-2">
+		<div>
+			<h5 class="mb-0 fw-bold">Data Fakultas</h5>
+		</div>
+		<a class="btn btn-primary btn-lg fw-bold" href="<?php echo base_url('fakultas/tambah') ?>">Tambah</a>
+	</div>
+
+	<div class="card-body">
+		<div class="table-responsive">
+			<table id="datatable" class="table table-striped table-bordered align-middle w-100 mb-0">
+				<thead class="table-dark">
+					<tr>
+						<td>No.</td>
+						<td>ID Fakultas</td>
+						<td>Nama Fakultas</td>
+						<td>Aksi</td>
+					</tr>
+				</thead>
+
+				
+
+			</table>
+		</div>
+	</div>
+</div>
