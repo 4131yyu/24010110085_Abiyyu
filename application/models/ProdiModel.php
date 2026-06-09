8b7613c3ccebb5ff2c576330cdc68b4b0a72922f
Abiyyu <abiyyu18@gmail.com>
2026-06-09 13:37:09 +0800

    feat: membuat model prodi dan get prodi

---
+++ application/models/ProdiModel.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ProdiModel extends CI_Model {
+
+    public function getAll()
+    {
+        $this->db->select('prodi.*, fakultas.fakultas_name');
+        $this->db->from('prodi');
+        $this->db->join('fakultas', 'fakultas.fakultas_id = prodi.fakultas_id');
+
+        return $this->db->get()->result_array();
+    }
+
+    public function getById($id)
+    {
+        return $this->db->get_where('prodi', [
+            'prodi_id' => $id
+        ])->row_array();
+    }
+    
+}
